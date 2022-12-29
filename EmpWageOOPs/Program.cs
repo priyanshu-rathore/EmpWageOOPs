ffa2c7cfdb2b2f4a959fd10dabccc3ae03abd174
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-29 22:12:39 +0530

    Ability to manage Employee wage of multiple companies using interface approach

--- EmpWageOOPs/Program.cs
+++ EmpWageOOPs/Program.cs
@@ -3,13 +3,10 @@
 namespace EmpWageOOPs{
     class Program{
         public static void Main(){
-            EmployeeWage dmart = new EmployeeWage("Dmart",20,2,10);
-            EmployeeWage Reliance = new EmployeeWage("Reliance",10,4,20);
-
-            dmart.computeEmpWage();
-            System.Console.WriteLine(dmart.toString());
-            Reliance.computeEmpWage();
-            System.Console.WriteLine(Reliance.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart",20,2,10);
+            empWageBuilder.addCompanyEmpWage("Reliance",10,4,20);
+            empWageBuilder.computeEmpWage();
 
         }
     }
