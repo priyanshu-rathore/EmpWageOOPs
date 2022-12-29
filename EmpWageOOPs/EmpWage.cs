ca51e2c1892b822892e05cbc41bd2294bca4d5ee
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-29 14:47:30 +0530

    Emp wage of multiple companies

--- EmpWageOOPs/EmpWage.cs
+++ EmpWageOOPs/EmpWage.cs
@@ -4,42 +4,38 @@ namespace EmpWageOOPs
 {
     internal class EmployeeWage
     {
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
-        public const int EMP_RATE_PER_HR = 20;
-        public const int MAX_WORKING_DAYS = 20;
-        public const int MAX_WORKING_HRS = 80;
-        public static void EmpWage()
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalHrs = 0;
-
-            //UC-1 Check employee present or not
-            Random random = new Random();
-
-            while (day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
-                int employeeInput = random.Next(0, 3);
-                //UC-4- Switch case
-                switch (employeeInput)
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
                 {
-                    case FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case PART_TIME:
+                    case IS_PART_TIME:
                         empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                //UC-2 calculate EmployeeWage
-                empWage = EMP_RATE_PER_HR * empHrs;
-                totalWage += empWage; //totalWage=totalWage+empWage
-                totalHrs += empHrs;
-                day++;
+
+                totalEmpHrs += empHrs;
+                System.Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
             }
-            Console.WriteLine("Total wage for {0} days:{1} and Hrs:{2}", (day - 1), totalWage, (totalHrs - empHrs));
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            System.Console.WriteLine("Total Emp Wage for company : " + company + " is : " + totalEmpWage);
+            return totalEmpWage;
         }
     }
 }
--- EmpWageOOPs/Program.cs
+++ EmpWageOOPs/Program.cs
@@ -3,7 +3,8 @@
 namespace EmpWageOOPs{
     class Program{
         public static void Main(){
-            EmployeeWage.EmpWage();
+            EmployeeWage.computeEmpWage("Adobe",20,2,10);
+            EmployeeWage.computeEmpWage("Adani",10,4,20);
         }
     }
 }
